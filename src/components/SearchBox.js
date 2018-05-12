fc02c1fc099b5ebf4ab11b4f07a1fe0894e7ed10
Bruce Schubert <bruce@emxsys.com>
2018-05-12 10:42:57 -0700

    Minor edits

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,11 +3,10 @@ import PropTypes from 'prop-types';
 
 import Globe from './Globe'
 
-export default class SerchBox extends Component {
+export default class SearchBox extends Component {
 
 
     render() {
-        // Create a Bootstrap card that renders the base and overlay layer lists
         return (
             <form id="search" className="form-inline">
                 <input id="searchText" className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search"/>
