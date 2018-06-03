68f47b914c722a43d52a147bdcf4b7cbb60ad2e9
Bruce Schubert <bruce@emxsys.com>
2018-06-03 15:57:38 -0700

    Refactored to use worldwind-react-globe-bs4 components.

--- src/index.js
+++ src/index.js
@@ -1,22 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import $ from 'jquery';
+/* 
+ * Copyright (c) 2018 Bruce Schubert.
+ * The MIT License
+ * http://www.opensource.org/licenses/mit-license
+ */
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './index.css'
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
+import App from './App'
 
-import App from './App';
-
-$(document).ready(function () {
-    // Auto-collapse the mobile main menu when its button items are clicked
-    $('.navbar-collapse a[role="button"]').click(function () {
-        $('.navbar-collapse').collapse('hide');
-    });
-    // Collapse card ancestors when the close icon is clicked
-    $('.collapse .close').on('click', function () {
-        $(this).closest('.collapse').collapse('hide');
-    });
-});
-
-// Render the App into the #root element
-ReactDOM.render(<App />, document.getElementById('app-root'));
+ReactDOM.render(<App />, document.getElementById('root'))
